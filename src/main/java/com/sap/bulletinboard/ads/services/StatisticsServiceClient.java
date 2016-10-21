6dc7c75af842ffd29b8f39b3bc33c71aad4f8122
Nena Raab <nena.raab@sap.com>
2016-10-22 01:49:21 +0200

    Solution Exercise 24: Make application secure
    history
    - Update README.md
    - Make use of XSUAA which is bound to cc-sap ID Zone
    - Delete defaultJwtToken.txt
    - Simplify WebSecurityConfig: incorporate feedback Ulrich Bestfleisch
    - Allow unauthenticated users to access these endpoints:
      - the DefaultController at "/" and the "health" endpoint
    - fix /WebSecurityConfig.java
    - Add a negative test case to check whether app is secure close #40
    - Add a negative test case to check whether app is secure close (fix #40)
    - Make sure, that /health endpoint is accessable by anyone
    - Add info about how to create logging-service instance
    - remove test for "health" actuator endpoint
    - add rabbitmq service as required service
    - Prepare application for multi tenant usage
    - Switch from `trial` to `cloudcourse`
    https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/741
    - Update Paging
      https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/651
    - increase version of xs security client library
    - fix xsuaa VCAP_SERVICES string
    - Demos how to add tenant info to MessageQueue and Logger MDC context
    - needs to increase version of xs security client library (0.24.17)
    - Switch from `cloudcourse` to new Trial Subaccount #760
      https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/760
      Change Org and IDZone

--- src/main/java/com/sap/bulletinboard/ads/services/StatisticsServiceClient.java
+++ src/main/java/com/sap/bulletinboard/ads/services/StatisticsServiceClient.java
@@ -9,13 +9,15 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.DefaultMessagePropertiesConverter;
 import org.springframework.stereotype.Component;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandKey;
 import com.sap.hcp.cf.logging.common.LogContext;
+import com.sap.xs2.security.container.SecurityContext;
+import com.sap.xs2.security.container.UserInfo;
+import com.sap.xs2.security.container.UserInfoException;
 
 @Component // defines a Spring Bean with name "statisticsServiceClient"
 public class StatisticsServiceClient {
@@ -29,38 +31,38 @@ public class StatisticsServiceClient {
     public StatisticsServiceClient(AmqpAdmin amqpAdmin, RabbitTemplate rabbitTemplate) {
         amqpAdmin.declareQueue(new Queue(ROUTING_KEY)); // creates queue, if not existing
         this.rabbitTemplate = rabbitTemplate;
-
-        /* Workaround till spring-rabbit 2.0 release */
-        DefaultMessagePropertiesConverter messagePropertiesConverter = new DefaultMessagePropertiesConverter();
-        messagePropertiesConverter
-                .setCorrelationIdAsString(DefaultMessagePropertiesConverter.CorrelationIdPolicy.STRING);
-        this.rabbitTemplate.setMessagePropertiesConverter(messagePropertiesConverter);
     }
 
-    public void advertisementIsShown(long id) {
+    public void advertisementIsShown(long id) throws UserInfoException {
         new IncrementCounterCommand(id).queue(); // queue calls the run() asynchronously
     }
 
     private class IncrementCounterCommand extends HystrixCommand<Void> {
         protected final String correlationId;
-        private String id;
+        private Long id;
+        private UserInfo userInfo;
+        private static final String TENANT = "tenant";
 
-        IncrementCounterCommand(long id) {
+        IncrementCounterCommand(Long id) throws UserInfoException {
             super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(StatisticsServiceClient.class.getName()))
                     .andCommandKey(HystrixCommandKey.Factory.asKey(ROUTING_KEY)));
-            this.id = String.valueOf(id);
+            this.id = id;
             this.correlationId = LogContext.getCorrelationId();
+            this.userInfo = SecurityContext.getUserInfo();
         }
 
         @Override
         protected Void run() throws Exception {
+            String tenant = userInfo.getIdentityZone();
             LogContext.initializeContext(correlationId);
+            LogContext.add(TENANT, tenant);
 
             logger.info("sending message '{}' for routing key '{}'", id, ROUTING_KEY);
-
-            rabbitTemplate.convertAndSend(null, ROUTING_KEY, id, new MessagePostProcessor() {
+            
+            rabbitTemplate.convertAndSend(ROUTING_KEY, id, new MessagePostProcessor() {
                 public Message postProcessMessage(Message message) {
-                    message.getMessageProperties().setCorrelationIdString(correlationId);
+                    message.getMessageProperties().setCorrelationIdString(LogContext.getCorrelationId());
+                    message.getMessageProperties().setHeader(TENANT, tenant);
                     return message;
                 }
             });
