ece0ff44fd476bb502ec829f1459efb7dbeabee2
Nena Raab <nena.raab@sap.com>
2016-10-22 01:18:05 +0200

    Solution Exercise 22: Deploy App Router
    History
    - update approuter version
    - make use of XSUAA which is bound to cc-sap ID Zone
    - increased approuter version
    - update xs-app.json map approuter/ads to ads/
    - apply multi-tenancy
    - load approuter from https://npm.sap.com
      https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/759
    - package.json let Cloud Foundry do the `npm install`

--- src/main/java/com/sap/bulletinboard/ads/controllers/DefaultController.java
+++ src/main/java/com/sap/bulletinboard/ads/controllers/DefaultController.java
@@ -2,6 +2,7 @@ package com.sap.bulletinboard.ads.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
     @GetMapping
-    public String get() {
-
-        return "ok";
+    public String get(@RequestHeader("Authorization") String authorization ) {
+         //TODO DO NOT EXPOSE THIS DATA IN PRODUCTION!!!
+//         return "<!DOCTYPE HTML>" +
+//             "<html>" +
+//             "<head></head>" +
+//             "<body>" +
+//             "Please decode the JWT token <a href = \"https://jwt.io\" target=\"_new\">here</a><br><br>" +
+//             authorization +
+//             "</body>" +
+//             "</html>";
+        return "OK";
     }
     
     @GetMapping("/instance-index")
